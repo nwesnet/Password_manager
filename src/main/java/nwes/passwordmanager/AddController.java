707e40fbe9da863995e03d7b99c8513f6415b43b
nwesnet <sss22872@gmail.com>
2025-04-01 17:22:34 +0200

    I finaly refactored the add button view

--- src/main/java/nwes/passwordmanager/AddController.java
+++ src/main/java/nwes/passwordmanager/AddController.java
@@ -1,10 +1,10 @@
 package nwes.passwordmanager;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
@@ -66,12 +66,24 @@ public class AddController {
         }
     }
     private void showAccountForm(){
+        // clear vbox
         vbAdd.getChildren().clear();
+        // create gridpane, set spacing and padding, implement column sizes
+        GridPane grid = new GridPane();
+        grid.setHgap(1);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(20));
+
+        ColumnConstraints labelColumn = new ColumnConstraints(80);
+        ColumnConstraints inputColumn = new ColumnConstraints(240);
+        grid.getColumnConstraints().addAll(labelColumn, inputColumn);
 
         accountResourceField = new TextField();
         accountResourceField.setPrefColumnCount(24);
+
         accountUsernameField = new TextField();
         accountUsernameField.setPrefColumnCount(24);
+
         accountPasswordField = new PasswordField();
         accountPasswordField.setPrefColumnCount(24);
 
@@ -84,19 +96,35 @@ public class AddController {
         cancelButton = new Button("Cancel");
         cancelButton.setOnAction(e -> onCancelButtonClick());
 
-        VBox formBox = new VBox(10,
-                new HBox(5, new Label("Resource:   "), accountResourceField),
-                new HBox(5, new Label("Username: "), accountUsernameField),
-                new HBox(5, new Label("Password:  "), accountPasswordField),
-                new HBox(10, saveButton, cancelButton)
-        );
+        grid.add(new Label("Resource:"), 0, 0);
+        grid.add(accountResourceField, 1, 0);
 
-        vbAdd.getChildren().addAll(formBox);
+        grid.add(new Label("Login:"), 0, 1);
+        grid.add(accountUsernameField, 1, 1);
+
+        grid.add(new Label("Password:"), 0, 2);
+        grid.add(accountPasswordField, 1, 2);
+
+        HBox hb = new HBox(5,
+                saveButton, cancelButton
+        );
+        hb.setAlignment(Pos.CENTER_LEFT);
+
+        vbAdd.getChildren().addAll(grid, hb);
     }
 
     private void showCardForm(){
         // I clear the vbox that I use for flexible interface
         vbAdd.getChildren().clear();
+        // Create gridpane, set spacing and padding, implement column size
+        GridPane grid = new GridPane();
+        grid.setHgap(1);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(10));
+
+        ColumnConstraints labelColumn = new ColumnConstraints(80);
+        ColumnConstraints inputColumn = new ColumnConstraints(240);
+        grid.getColumnConstraints().addAll(labelColumn, inputColumn);
         // I define the fields and set up it
         cardResourceField = new TextField();
         cardResourceField.setPrefColumnCount(24);
@@ -108,7 +136,7 @@ public class AddController {
         cardDateField.setPrefColumnCount(6);
 
         cardCVVField = new TextField();
-        cardCVVField.setPrefColumnCount(6);
+        cardCVVField.setPrefColumnCount(10);
 
         cardNameField = new TextField();
         cardNameField.setPrefColumnCount(20);
@@ -136,23 +164,42 @@ public class AddController {
         cancelButton = new Button("cancel");
         cancelButton.setOnAction(e -> onCancelButtonClick());
 
-        VBox formBox = new VBox(10,
-                new HBox(5, new Label("Resource: "), cardResourceField),
-                new HBox(5, new Label("Card number: "), cardNumberField),
-                new HBox(5, new Label("Date:                 "), cardDateField, new Label("          CVV: "), cardCVVField),
-                new HBox(5, new Label("Name:              "), cardNameField),
+        grid.add(new Label("Resource:"), 0, 0);
+        grid.add(cardResourceField, 1, 0);
+
+        grid.add(new Label("Number:"), 0, 1);
+        grid.add(cardNumberField, 1, 1);
+
+        grid.add(new Label("Date:"), 0, 2);
+        grid.add(cardDateField, 1, 2);
+
+        grid.add(new Label("Owner:"), 0, 3);
+        grid.add(cardNameField, 1, 3);
+
+        VBox formBox = new VBox(5,
+                new HBox(5, new Label("CVV:                   "), cardCVVField),
                 new HBox(5,new Label("Pin:                    "), cardPinField),
                 new HBox(5, new Label("Pay network:   "), cardTypePayField),
                 new HBox(5, new Label("Card type:        "), cardTypeField),
-                new HBox(10, saveButton, cancelButton)
+                new HBox(5, saveButton, cancelButton)
         );
 
         // I add the items in VBox
-        vbAdd.getChildren().addAll(formBox);
+        vbAdd.getChildren().addAll(grid, formBox);
     }
 
     private void showLinkForm() {
+        // Clear vbox
         vbAdd.getChildren().clear();
+        // Create gridpane, set spacing and padding, implement column size
+        GridPane grid = new GridPane();
+        grid.setHgap(1);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(20));
+
+        ColumnConstraints labelColumn = new ColumnConstraints(80);
+        ColumnConstraints inputColumn = new ColumnConstraints(240);
+        grid.getColumnConstraints().addAll(labelColumn, inputColumn);
 
         TextField resourceField = new TextField();
         TextField linkField = new TextField();
@@ -171,13 +218,18 @@ public class AddController {
 
         cancelButton.setOnAction(e -> onCancelButtonClick());
 
+        grid.add(new Label("Resource:"), 0, 0);
+        grid.add(resourceField, 1, 0);
+
+        grid.add(new Label("Link:"), 0, 1);
+        grid.add(linkField, 1, 1);
+
         VBox formBox = new VBox(10,
-                new HBox(5, new Label("Resource: "), resourceField),
-                new HBox(5, new Label("Link:          "), linkField),
                 new HBox(10, saveButton, cancelButton)
         );
 
-        vbAdd.getChildren().add(formBox);
+
+        vbAdd.getChildren().addAll(grid, formBox);
     }
     private void showWalletForm(){
         vbAdd.getChildren().clear();
